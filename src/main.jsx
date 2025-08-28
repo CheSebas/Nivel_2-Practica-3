a1878dd383ea194553d801d2d507a3977ff3888a
CheSebas <rodulfsebas@hotmail.com>
2025-08-28 11:53:28 -0500

    Deploy

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Nivel_2-Practica-3">
       <App />
     </BrowserRouter>
   </StrictMode>
